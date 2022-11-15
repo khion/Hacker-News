c909a36d0a20892b659c00578c0dd71bac8b0415
yourusername <nick_gayem@yahoo.com>
2022-11-14 19:37:34 -0800

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Rodnick Gayem on 2022-11-14.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
